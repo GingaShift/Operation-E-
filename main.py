3914e739ab0a7a7e59534ded32dabf4fa5c2c01f
King_of_evoli <gabriellallier05@gmail.com>
2024-03-01 14:47:33 +0100

    essaie de gabi

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import pygame
 import random
 from joueur import*
 from boss import*
-print("Essaie")
-print("essaie2")
-print("Ucb")
-print("Genshin impact>>> les autres jeux")
+pygame.init()
+
+pygame.display.set_caption("Opération E")
+pygame.display.set_mode((500,450))
+
+running=True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+            pygame.quit()
+            print('Fin du Jeu')
