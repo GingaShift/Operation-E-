5f2faed94ec82f8a7d86ae8235df709f62081976
King_of_evoli <gabriellallier05@gmail.com>
2024-03-07 18:20:03 +0100

    essaie de gabi

--- joueur.py
+++ joueur.py
@@ -2,18 +2,21 @@ import pygame
 import boss
 
 class player:
-    def __init__(self,vie,force,velocity):
-        self.vie=vie
-        self.force=force
-        self.image=pygame.image.load('faux trouver le esign')
+    def __init__(self,pseudo):
+        self.pseudo=pseudo
+        self.vie=60
+        self.force=10
+        self.image=pygame.image.load("Chemin vers l'image")
         self.rect=self.image.get_rect()
         self.rect.x=20
         self.rect.y=20
-        self.velocity=velocity
+        self.velocity=20
 
     def __del__(self):
         pass
 
+    def repondre(self,reponse):
+        return reponse
     def move_right(self):
         self.rect.x+=self.velocity
     def move_left(self):
