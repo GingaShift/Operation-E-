09aca7a52a582416825ab80f6e1acfc0efbf4c6c
gabri <gabriellallier05@gmail.com>
2024-05-28 22:20:17 +0200

    fonction start

--- main.py
+++ main.py
@@ -12,59 +12,59 @@ background = pygame.image.load('pictures/bg.jpg')
 game=game.Game()
 
 running=True
+def starting_benwars():
+    while running and game.gamer.health>0 and game.gamer.score < 500:
 
-while running and game.gamer.health>0 and game.gamer.score < 500:
+        screen.blit(background, (0,-200))
+        screen.blit(game.gamer.image, game.gamer.rect)
 
-    screen.blit(background, (0,-200))
-    screen.blit(game.gamer.image, game.gamer.rect)
+        myfont = pygame.font.SysFont("monospace", 62)
+        score_display = myfont.render(str(game.gamer.score), 1, (255, 255, 255))
+        screen.blit(score_display, (0, 0))
 
-    myfont = pygame.font.SysFont("monospace", 62)
-    score_display = myfont.render(str(game.gamer.score), 1, (255, 255, 255))
-    screen.blit(score_display, (0, 0))
+        game.gamer.update_health_bar(screen)
 
-    game.gamer.update_health_bar(screen)
+        for projectile in game.gamer.all_projectiles:
+            projectile.move()
 
-    for projectile in game.gamer.all_projectiles:
-        projectile.move()
+        for vetn in game.all_vetneufs:
+            vetn.forward()
+            vetn.update_health_bar(screen)
 
-    for vetn in game.all_vetneufs:
-        vetn.forward()
-        vetn.update_health_bar(screen)
+        for veto in game.all_vetold:
+            veto.forward()
+            veto.update_health_bar(screen)
 
-    for veto in game.all_vetold:
-        veto.forward()
-        veto.update_health_bar(screen)
+        for obs_etg in game.all_obs:
+            obs_etg.forward()
 
-    for obs_etg in game.all_obs:
-        obs_etg.forward()
+        game.gamer.all_projectiles.draw(screen)
 
-    game.gamer.all_projectiles.draw(screen)
+        game.all_vetneufs.draw(screen)
+        game.all_vetold.draw(screen)
+        game.all_obs.draw(screen)
 
-    game.all_vetneufs.draw(screen)
-    game.all_vetold.draw(screen)
-    game.all_obs.draw(screen)
+        if game.pressed.get(pygame.K_RIGHT) and game.gamer.rect.x + game.gamer.rect.width < screen.get_width():
+            game.gamer.move_right()
+        elif game.pressed.get(pygame.K_LEFT) and game.gamer.rect.x > 0:
+            game.gamer.move_left()
+        elif game.pressed.get(pygame.K_UP) and game.gamer.rect.y > 0:
+            game.gamer.move_up()
+        elif game.pressed.get(pygame.K_DOWN) and game.gamer.rect.y + game.gamer.rect.height < screen.get_height():
+            game.gamer.move_down()
 
-    if game.pressed.get(pygame.K_RIGHT) and game.gamer.rect.x + game.gamer.rect.width < screen.get_width():
-        game.gamer.move_right()
-    elif game.pressed.get(pygame.K_LEFT) and game.gamer.rect.x > 0:
-        game.gamer.move_left()
-    elif game.pressed.get(pygame.K_UP) and game.gamer.rect.y > 0:
-        game.gamer.move_up()
-    elif game.pressed.get(pygame.K_DOWN) and game.gamer.rect.y + game.gamer.rect.height < screen.get_height():
-        game.gamer.move_down()
+        pygame.display.flip()
 
-    pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                game.pressed[event.key] = True
 
-        elif event.type == pygame.KEYDOWN:
-            game.pressed[event.key] = True
+                if event.key == pygame.K_SPACE:
+                    game.gamer.launch_projectile()
 
-            if event.key == pygame.K_SPACE:
-                game.gamer.launch_projectile()
-
-        elif event.type == pygame.KEYUP:
-            game.pressed[event.key] = False
+            elif event.type == pygame.KEYUP:
+                game.pressed[event.key] = False
